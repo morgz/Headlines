598c9b5296b2b6bd81b85d057d044bf37a815c02
Daniel Morgan <danmorgz@gmail.com>
2016-01-18 11:37:07 +0000

    guardian api manager with one method to get fintech articles added

---
+++ Headlines/GuardianAPIManager.swift
@@ -0,0 +1,111 @@
+//
+//  GuardianAPIManager.swift
+//  Headlines
+//
+//  Created by Daniel Morgz on 18/01/2016.
+//  Copyright © 2016 Daniel Morgan. All rights reserved.
+//
+import Foundation
+import UIKit
+import Alamofire
+
+class GuardianAPIManager: NSObject {
+
+    static let apiKey = "nj8pstqu5yat6yesfsdmd39"
+    static let baseURL = "http://content.guardianapis.com/"
+    
+    var manager:Alamofire.Manager!
+    
+    class var sharedInstance:GuardianAPIManager {
+        return GuardianAPIManagerSharedInstance
+    }
+    
+    override init() {
+        super.init()
+        
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        self.manager = Alamofire.Manager(configuration: configuration)
+    }
+}
+
+let GuardianAPIManagerSharedInstance = GuardianAPIManager()
+
+
+extension GuardianAPIManager {
+    
+    
+    func getFinTechArticles() -> Request {
+        return manager.request(.GET, GuardianAPIManager.baseURL+"search?q=fintech&show-fields=main,body"+"&api-key=\(GuardianAPIManager.apiKey)")
+            .validate()
+    }
+    
+}
+
+
+
+
+//Mark: Generics can be used to provide automatic, type-safe response object serialization.
+
+public protocol ResponseObjectSerializable {
+    init?(response: NSHTTPURLResponse, representation: AnyObject)
+}
+
+extension Request {
+    public func responseObject<T: ResponseObjectSerializable>(completionHandler: Response<T, NSError> -> Void) -> Self {
+        let responseSerializer = ResponseSerializer<T, NSError> { request, response, data, error in
+            guard error == nil else { return .Failure(error!) }
+            
+            let JSONResponseSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
+            let result = JSONResponseSerializer.serializeResponse(request, response, data, error)
+            
+            switch result {
+            case .Success(let value):
+                if let
+                    response = response,
+                    responseObject = T(response: response, representation: value["response"])
+                {
+                    return .Success(responseObject)
+                } else {
+                    let failureReason = "JSON could not be serialized into response object: \(value)"
+                    let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
+                    return .Failure(error)
+                }
+            case .Failure(let error):
+                return .Failure(error)
+            }
+        }
+        
+        return response(responseSerializer: responseSerializer, completionHandler: completionHandler)
+    }
+}
+
+
+public protocol ResponseCollectionSerializable {
+    static func collection(response response: NSHTTPURLResponse, representation: AnyObject) -> [Self]
+}
+
+extension Alamofire.Request {
+    public func responseCollection<T: ResponseCollectionSerializable>(completionHandler: Response<[T], NSError> -> Void) -> Self {
+        let responseSerializer = ResponseSerializer<[T], NSError> { request, response, data, error in
+            guard error == nil else { return .Failure(error!) }
+            
+            let JSONSerializer = Request.JSONResponseSerializer(options: .AllowFragments)
+            let result = JSONSerializer.serializeResponse(request, response, data, error)
+            
+            switch result {
+            case .Success(let value):
+                if let response = response {
+                    return .Success(T.collection(response: response, representation: value["response"]))
+                } else {
+                    let failureReason = "Response collection could not be serialized due to nil response"
+                    let error = Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
+                    return .Failure(error)
+                }
+            case .Failure(let error):
+                return .Failure(error)
+            }
+        }
+        
+        return response(responseSerializer: responseSerializer, completionHandler: completionHandler)
+    }
+}
