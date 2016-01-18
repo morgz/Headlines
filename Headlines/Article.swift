98865d83994cb5740910cfc4dd2f1d2cdf6126c4
Daniel Morgan <danmorgz@gmail.com>
2016-01-18 11:32:04 +0000

    add basic article realm model

---
+++ Headlines/Article.swift
@@ -0,0 +1,25 @@
+//
+//  Article.swift
+//  Headlines
+//
+//  Created by Daniel Morgz on 18/01/2016.
+//  Copyright © 2016 Daniel Morgan. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Article: Object {
+    
+    dynamic var id = ""
+    dynamic var title = ""
+    dynamic var bodyText = ""
+    
+    dynamic var publishDate:NSDate?
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
