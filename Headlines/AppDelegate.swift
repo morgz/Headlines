fc13de810b46783a9addeb4ce39e992a681e8790
Daniel Morgan <danmorgz@gmail.com>
2016-01-18 12:11:05 +0000

    amanage migrations and create realm db

--- Headlines/AppDelegate.swift
+++ Headlines/AppDelegate.swift
@@ -19,10 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         //Test getting our articles
+        self.setupRealm()
         ArticleManager.sharedInstance.getRemoteArticles()
         
         return true
     }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -45,7 +47,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    //MARK: Realm
+    
+    func setupRealm() {
+        self.realmMigrations()
+        self.uiRealm = try! Realm()
+        // for swift 2.0 Xcode 7
+        print("Realm DB Path:"+"\n"+self.uiRealm.path+"\n") //Print out the path to the realm db so we can easily fire up a realm browser to look at data
+    }
+    
+    //If you make a change that needs a migration then bump the schema version otherwise you'll see an ERRRRRRRRROR
+    func realmMigrations() {
+        // Inside your application(application:didFinishLaunchingWithOptions:)
+        
+        let config = Realm.Configuration(
+            // Set the new schema version. This must be greater than the previously used
+            // version (if you've never set a schema version before, the version is 0).
+            schemaVersion: 1,
+            
+            // Set the block which will be called automatically when opening a Realm with
+            // a schema version lower than the one set above
+            migrationBlock: { migration, oldSchemaVersion in
+                // We haven’t migrated anything yet, so oldSchemaVersion == 0
+                if (oldSchemaVersion < 1) {
+                    // Nothing to do!
+                    // Realm will automatically detect new properties and removed properties
+                    // And will update the schema on disk automatically
+                }
+        })
+        
+        // Tell Realm to use this new configuration object for the default Realm
+        Realm.Configuration.defaultConfiguration = config
+    }
 
 }
 
