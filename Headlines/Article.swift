210c56529a859e2248c21f6330456c8e7078288c
Daniel Morgan <danmorgz@gmail.com>
2016-01-18 11:44:21 +0000

    add methods to article for serilising the object from api

--- Headlines/Article.swift
+++ Headlines/Article.swift
@@ -26,5 +26,30 @@ final class Article: Object {
 //  override static func ignoredProperties() -> [String] {
 //    return []
 //  }
+    
+
+    //Mark: Creating a representation from the API
+    convenience required init?(response: NSHTTPURLResponse, representation: AnyObject) {
+        self.init()
+        //self.username = response.URL!.lastPathComponent!
+        self.id = representation.valueForKeyPath("id") as! String
+        self.title = representation.valueForKeyPath("title") as! String
+        
+    }
+    
+    //Creates multiple representations of an object
+    static func collection(response response: NSHTTPURLResponse, representation: AnyObject) -> [Article] {
+        var remoteObjects: [Article] = []
+        
+        if let representation = representation as? [[String: AnyObject]] {
+            for eventRepresentation in representation {
+                if let report = Article(response: response, representation: eventRepresentation) {
+                    remoteObjects.append(report)
+                }
+            }
+        }
+        
+        return remoteObjects
+    }
 
 }
