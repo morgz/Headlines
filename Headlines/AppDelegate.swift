60b8ac998cc6ab3382a9e0ec329cec28258318c8
Daniel Morgan <danmorgz@gmail.com>
2016-01-19 13:21:30 +0000

    removing fav cells properly and safely

--- Headlines/AppDelegate.swift
+++ Headlines/AppDelegate.swift
@@ -102,3 +102,12 @@ extension UIViewController {
     }
 }
 
+func delay(delay:Double, closure:()->()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure)
+}
+
